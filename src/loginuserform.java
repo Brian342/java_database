965937682a936e3b69c7aa1bb8851a24ebcf9ed7
Brian kimanzi <migelbrian3@gmail.com>
2024-11-07 12:35:14 +0300

    created the login form

--- src/loginuserform.java
+++ src/loginuserform.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
+import javax.xml.stream.events.StartElement;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class loginuserform extends JDialog{
     private JTextField tfEmail;
@@ -16,12 +20,89 @@ public class loginuserform extends JDialog{
         setModal(true);
         setLocationRelativeTo(parent); //display dialog in middle of frame
         setDefaultCloseOperation(DISPOSE_ON_CLOSE); // terminate dialog when click on close button
+
+
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = tfEmail.getText();
+                String password = String.valueOf(pfPassword.getPassword());
+
+                user = getAuthenticateUser(email, password);
+
+                if(user != null){
+                    dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(loginuserform. this,
+                            "Email or Password invalid",
+                            "Try Again",
+                            JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+        btnCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
         setVisible(true); // makes dialog visible
+    }
+    public User user;
+    private User getAuthenticateUser(String email, String password){
+        User user = null;
+        final String DB_URL = "jdbc:mysql://localhost:3306/Javaconnectivity"; // sql connectivity
+        final String USERNAME = "root";
+        final String PASSWORD = "";
 
+        try{
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
 
+            // checks if user exist on the database
+            Statement stmt = conn.createStatement();
+            String sql = "SELECT * FROM users WHERE email=? AND password=?";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if(resultSet.next()){
+                user = new User();
+                user.name = resultSet.getString("name");
+                user.email = resultSet.getString("email");
+                user.phone = resultSet.getString("phone");
+                user.password = resultSet.getString("password");
+            }
+
+            stmt.close();
+            conn.close();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
     public static void main(String[] args) {
         loginuserform loginform = new loginuserform(null);
+        User user = loginform.user; // if the user credentials are correct, then the authentication is complete.
+        if(user != null){
+            JOptionPane.showMessageDialog(null,
+                    "Successful Authentication of: " + user.name
+                    + "\nEmail: " + user.email
+                    +"\nphone:" + user.phone,
+                    "\nLogin Success",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+
+
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"Authentication Canceled", "Authetication Canceled", JOptionPane.INFORMATION_MESSAGE);
+
+        }
     }
 }
